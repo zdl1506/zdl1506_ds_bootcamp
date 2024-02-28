c2989ad04544f2b416a9113fee2254bfc3212899
zdl1506 <152559640+zdl1506@users.noreply.github.com>
2024-02-28 14:19:54 +0000

    Update replace.py

--- replace.py
+++ replace.py
@@ -1,15 +1,11 @@
-#Save the sentence as a single string
-#Replace exclamation marks with blank spaces
-#Print the modified sentence with spaces
-#Convert the sentence to uppercase
-#Print the sentence in uppercase
-#Print the sentence in reverse
-
+# Replace exclamation marks with blank spaces
 sentence_1 = "The!quick!brown!fox!jumps!over!the!lazy!dog."
 sentence_2 = sentence_1.replace("!"," ") 
 print(sentence_2)
 
+# Convert the sentence to uppercase
 sentence_3 = sentence_2.upper()
 print(sentence_3)
 
+#Print the sentence in reverse
 print(sentence_3[ : :-1])
