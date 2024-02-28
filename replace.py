f9a8fd0e0adb71110401679ec58d167da44cabf9
zdl1506 <152559640+zdl1506@users.noreply.github.com>
2024-02-28 14:48:20 +0000

    Update replace.py

--- replace.py
+++ replace.py
@@ -1,11 +1,5 @@
-# Replace exclamation marks with blank spaces
+# Original sentence
 sentence_1 = "The!quick!brown!fox!jumps!over!the!lazy!dog."
-sentence_2 = sentence_1.replace("!"," ") 
-print(sentence_2)
 
-# Convert the sentence to uppercase
-sentence_3 = sentence_2.upper()
-print(sentence_3)
-
-#Print the sentence in reverse
-print(sentence_3[ : :-1])
+# Replacing exclamation marks with blank spaces, converting to uppercase, and printing the reversed sentence
+print(sentence_1.replace("!", " ").upper()[::-1])
